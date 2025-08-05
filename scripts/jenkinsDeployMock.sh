aea75a8ac55b06ec1dccdd17edc12eb34fc7cb84
Sandeep Kumar M V <venu.medidi@gmail.com>
2025-08-05 21:15:10 +0530

    Update jenkinsDeployMock.sh

--- scripts/jenkinsDeployMock.sh
+++ scripts/jenkinsDeployMock.sh
@@ -6,43 +6,64 @@
 #   To Clone Repo & Scan all Yaml files to deploy contracts to mock server
 #     ex: sh scripts/jenkinsDeployMock.sh http://localhost:8080 y
 #
-#   To Clone Repo & 'Scan only' Yaml files since last commit, deploy contracts to mock server
+#   To Clone Repo & Scan all Yaml files to deploy contracts to mock server
 #     ex: sh scripts/jenkinsDeployMock.sh http://localhost:8080
 
 MOCK_SERVER=$1
 SCAN_FULL_DIRECTORY=$2
+MOCK_SYSTEM="SMART-MOCK"
 
 CURRENT_DIRECTORY=$(pwd)
 dir=$(dirname "$0")"/"
 SCRIPT_PATH=$dir
 COMMON_PATH="$CURRENT_DIRECTORY/$SCRIPT_PATH/common"
-echo "Path : $0 $dir"
 
 # 2. Scan repo
 if [ "$SCAN_FULL_DIRECTORY" = "y" ] || [ "$SCAN_FULL_DIRECTORY" = "Y" ]; then
   YAML_FILE_PATHS=$(sh $COMMON_PATH/find_yaml_files_in_directory.sh $CURRENT_DIRECTORY)
+  echo ""
+  echo "All Files in repo:"
+  echo ""
+  echo "$YAML_FILE_PATHS"
+  echo ""
 else
   # Get latest commit hash
   LATEST_COMMIT=$(git rev-parse HEAD)
   # Collect added or modified YAML files in an array
-  YAML_FILE_PATHS=($(git diff-tree --no-commit-id --name-status -r "$LATEST_COMMIT" \
+  YAML_FILE_PATHS=$(git diff-tree --no-commit-id --name-status -r "$LATEST_COMMIT" \
   | awk '$1 == "A" || $1 == "M" {print $2}' \
-  | grep -E '\.ya?ml$'))
+  | grep -E '\.ya?ml$')
   cd ..
+  echo ""
+  echo "All Files in last commit:"
+  echo ""
+  echo "$YAML_FILE_PATHS"
+  echo ""
 fi
 
-finalStatus="["
-for file in "${YAML_FILE_PATHS[@]}"; do
+counter=1
+echo "$YAML_FILE_PATHS" | while IFS= read -r path; do
+  #echo "$counter. $path"
+  file=$path;
   if [ "$SCAN_FULL_DIRECTORY" = "y" ] || [ "$SCAN_FULL_DIRECTORY" = "Y" ]; then
     filePath=$file
   else
     filePath="$CURRENT_DIRECTORY/$file"
   fi
-  # Return status of all endpoints in a given yaml file.
-  status=$(sh "$COMMON_PATH"/yamlParser.sh $filePath $MOCK_SERVER)
-  finalStatus="$finalStatus $status,"
+
+  if [ -f "$filePath" ]; then
+    echo ""
+    echo "Configuring '$MOCK_SYSTEM' for $filePath"
+    echo ""
+    # Return status of all endpoints in a given yaml file.
+    status=$(sh "$COMMON_PATH"/yamlParser.sh $filePath $MOCK_SERVER)
+    echo ""
+    echo "Status: $status"
+    echo ""
+  fi
+  counter=$((counter + 1))
 done
-finalStatus="$finalStatus]"
-echo $finalStatus
+
+exit 0
 
 
